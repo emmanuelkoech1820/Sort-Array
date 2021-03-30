6d29fa6b1baea5e8045a3700d19b4ecbb9c8980c
Emmanuel Koech <EMMANUEL.KOECH@equitybank.co.ke>
2021-03-30 16:16:56 +0300

    Add project files.

---
+++ LinkedList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] array = new string [5] { "1", "2", "3", "4", "5" };
+            for(int i = array.Length-1; i>=0; i--)
+            {
+                Console.WriteLine(array[i]);
+            }
+            for(int i =0; i<array.Length-1; i++)
+            {
+                for (int j = i+1; j<array.Length; j++)
+                {
+                    var temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+
+            }
+            foreach(var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+}
